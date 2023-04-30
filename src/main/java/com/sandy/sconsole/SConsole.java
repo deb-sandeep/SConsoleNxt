febfa95b42c4408523bad1ba40d0c730097a6fbb
Sandeep Deb <deb.sandeep@gmail.com>
2023-04-30 23:31:06 +0530

    Database integration and JPA test setup

--- src/main/java/com/sandy/sconsole/SConsole.java
+++ src/main/java/com/sandy/sconsole/SConsole.java
@@ -13,8 +13,11 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
