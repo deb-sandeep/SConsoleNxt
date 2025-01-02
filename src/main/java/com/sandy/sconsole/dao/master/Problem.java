2aae860168ac4f4ad391b606fe8a3c4d3b11f9ed
Sandeep Deb <deb.sandeep@gmail.com>
2025-01-02 10:33:18 +0530

    Errata

--- src/main/java/com/sandy/sconsole/dao/master/Problem.java
+++ src/main/java/com/sandy/sconsole/dao/master/Problem.java
@@ -19,7 +19,7 @@ public class Problem {
     @ManyToOne( fetch = FetchType.LAZY, optional = false )
     @JoinColumns( { @JoinColumn( name = "book_id", referencedColumnName = "book_id", nullable = false ), @JoinColumn( name = "chapter_num", referencedColumnName = "chapter_num", nullable = false ) } )
     @OnDelete( action = OnDeleteAction.CASCADE )
-    private Chapter chapterMaster;
+    private Chapter chapter;
     
     @Column( name = "exercise_name", nullable = false, length = 64 )
     private String exerciseName;
